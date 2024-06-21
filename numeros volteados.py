f90e0b30ee5308ca55f8121ef99387afe93516a7
GuilleJ-RM <millanguillermo2504@gmail.com>
2024-06-21 13:46:35 -0300

    voltear numeros usando swap

---
+++ numeros volteados.py
@@ -0,0 +1,6 @@
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+
+num1, num2 = num2, num1
+
+print(f"El primer número ahora es {num1} y el segundo es {num2}")
